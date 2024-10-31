0d258b5d7fe0d47beb5b64938ecedecfd29d9341
MRUIZPTA <martin.ruiz@segw.com.ar>
2024-10-31 08:50:59 -0300

    final commit

--- src/components/footer.js
+++ src/components/footer.js
@@ -108,18 +108,7 @@ const Footer = () => {
         <a href="https://github.com/bchiang7/v4">
           <div>Designed &amp; Built by Martin Ruiz</div>
 
-          {githubInfo.stars && githubInfo.forks && (
-            <div className="github-stats">
-              <span>
-                <Icon name="Star" />
-                <span>{githubInfo.stars.toLocaleString()}</span>
-              </span>
-              <span>
-                <Icon name="Fork" />
-                <span>{githubInfo.forks.toLocaleString()}</span>
-              </span>
-            </div>
-          )}
+          {githubInfo.stars && githubInfo.forks && <div className="github-stats"></div>}
         </a>
       </StyledCredit>
     </StyledFooter>
